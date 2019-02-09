af267c28d9387e5b589f71199ce74b77d8975db4
André Vicente Milack <andrevicente.m@gmail.com>
2019-02-09 21:07:15 -0200

    Implement the conversion from CommandInput to Command

--- src/lib.rs
+++ src/lib.rs
@@ -86,23 +86,62 @@ pub enum DurationParseError {
     EmptyDuration,
 }
 
+pub struct InvalidGoalPeriod;
+
+pub enum CommandParseError {
+    DateTimeParseError(chrono::format::ParseError),
+    DurationParseError(DurationParseError),
+    GoalPeriodParseError(InvalidGoalPeriod),
+}
+
 impl<'a> Command<'a> {
     pub fn execute(&self) -> Result<(), ()> {
-        Err(()) // TODO: Implement!
+        unimplemented!()
     }
 }
 
 impl<'a> CommandInput<'a> {
     pub fn execute(&self) -> Result<(), ()> {
-        Err(()) // TODO: Implement!
+        unimplemented!()
     }
 }
 
 impl<'a> TryFrom<CommandInput<'a>> for Command<'a> {
-    type Error = ();
+    type Error = CommandParseError;
 
     fn try_from(input: CommandInput<'a>) -> Result<Command, Self::Error> {
-        Err(()) // TODO: Implement!
+        Ok(match input {
+            CommandInput::Enter { datetime } => Command::Enter {
+                datetime: ForgetableDateTime::try_from(datetime)?,
+            },
+            CommandInput::Exit { datetime } => Command::Exit {
+                datetime: ForgetableDateTime::try_from(datetime)?,
+            },
+            CommandInput::Create { mnemonic, code } => Command::Create { mnemonic, code },
+            CommandInput::Edit { mnemonic, code } => Command::Edit { mnemonic, code },
+            CommandInput::Delete { mnemonic } => Command::Delete { mnemonic },
+            CommandInput::Start { mnemonic, datetime } => Command::Start {
+                mnemonic,
+                datetime: ForgetableDateTime::try_from(datetime)?,
+            },
+            CommandInput::Stop { mnemonic, datetime } => Command::Stop {
+                mnemonic,
+                datetime: ForgetableDateTime::try_from(datetime)?,
+            },
+            CommandInput::Commit { mnemonic, datetime } => Command::Commit {
+                mnemonic,
+                datetime: parse_datetime_or_now(datetime)?,
+            },
+            CommandInput::Open { mnemonic } => Command::Open { mnemonic },
+            CommandInput::Resolve { mnemonic } => Command::Resolve { mnemonic },
+            CommandInput::Goal { period, goal_arg, mnemonic } => Command::Goal {
+                period: GoalPeriod::from_str(period)?,
+                goal_arg: GoalArg::try_from(goal_arg)?,
+                mnemonic,
+            },
+            CommandInput::Goals { mnemonic } => Command::Goals { mnemonic },
+            CommandInput::Status { mnemonic } => Command::Status { mnemonic },
+        })
     }
 }
 
@@ -111,9 +150,7 @@ impl<'a> TryFrom<ForgetableDateTimeInput<'a>> for ForgetableDateTime {
 
     fn try_from(input: ForgetableDateTimeInput<'a>) -> Result<ForgetableDateTime, Self::Error> {
         Ok(ForgetableDateTime {
-            datetime: input.datetime
-                .map(|s| DateTime::from_str(s))
-                .unwrap_or_else(|| Ok(Local::now()))?,
+            datetime: parse_datetime_or_now(input.datetime)?,
             forgot: input.forgot,
         })
     }
@@ -127,6 +164,44 @@ impl<'a> TryFrom<GoalArgInput<'a>> for GoalArg {
             GoalArgInput::Set(duration) => GoalArg::Set(parse_duration(duration)?),
             GoalArgInput::Erase => GoalArg::Erase,
         })
+    }
+}
+
+impl FromStr for GoalPeriod {
+    type Err = InvalidGoalPeriod;
+
+    fn from_str(string: &str) -> Result<Self, InvalidGoalPeriod> {
+        Ok(match string {
+            "month" => GoalPeriod::Month,
+            "week" => GoalPeriod::Week,
+            "day" => GoalPeriod::Day,
+            "sunday" => GoalPeriod::Weekday(Weekday::Sun),
+            "monday" => GoalPeriod::Weekday(Weekday::Mon),
+            "tuesday" => GoalPeriod::Weekday(Weekday::Tue),
+            "wednesday" => GoalPeriod::Weekday(Weekday::Wed),
+            "thursday" => GoalPeriod::Weekday(Weekday::Thu),
+            "friday" => GoalPeriod::Weekday(Weekday::Fri),
+            "saturday" => GoalPeriod::Weekday(Weekday::Sat),
+            _ => return Err(InvalidGoalPeriod),
+        })
+    }
+}
+
+impl From<chrono::format::ParseError> for CommandParseError {
+    fn from(error: chrono::format::ParseError) -> CommandParseError {
+        CommandParseError::DateTimeParseError(error)
+    }
+}
+
+impl From<DurationParseError> for CommandParseError {
+    fn from(error: DurationParseError) -> CommandParseError {
+        CommandParseError::DurationParseError(error)
+    }
+}
+
+impl From<InvalidGoalPeriod> for CommandParseError {
+    fn from(error: InvalidGoalPeriod) -> CommandParseError {
+        CommandParseError::GoalPeriodParseError(error)
     }
 }
 
@@ -151,6 +226,12 @@ impl<'a> From<&'a ArgMatches<'a>> for GoalArgInput<'a> {
             )
         }
     }
+}
+
+fn parse_datetime_or_now(input: Option<&str>) -> Result<DateTime, chrono::format::ParseError> {
+    input
+        .map(|s| DateTime::from_str(s))
+        .unwrap_or_else(|| Ok(Local::now()))
 }
 
 fn parse_duration(input: &str) -> Result<Duration, DurationParseError> {
